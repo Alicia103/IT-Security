ab9348d121f347fa6cadf8f2267fe0665ea173a4
Alicia103 <39268107+Alicia103@users.noreply.github.com>
2018-11-11 20:41:29 +0100

    Update guestbook.js

--- Exercise3_vulnerable/guestbook.js
+++ Exercise3_vulnerable/guestbook.js
@@ -1,8 +1,7 @@
 function addComment() {
-    // Get the input value
+    
     var value = $('#inp').val();
 
-    // This is the vulnerable comment
     $('#falseDiv').append("<ul style='list-style-type: none' id='newFalseList'></ul>");
     $("#newFalseList").append("<li>" + value + "</li>");
 }
